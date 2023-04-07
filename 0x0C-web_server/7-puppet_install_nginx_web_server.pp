3d6fbe4643eb1f59d02618530df7416aade2dba2
Meresa Gidey <111004235+Meresa24@users.noreply.github.com>
2023-04-07 13:49:42 +0200

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,40 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
-}
+#!/usr/bin/env bash
+# Installs, configures, and starts the server
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
+# the server's configuration
+SERVER_CONFIG=\
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
+        server_name _;
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+
+        if (\$request_filename ~ redirect_me){
+                rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+                root /var/www/error/;
+                internal;
+        }
+}"
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-enabled/default"
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+        service nginx start
+else
+        service nginx restart
+fi
